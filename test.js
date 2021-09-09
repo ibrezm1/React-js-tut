3d007e2c72924edebfb2f1018710227f51e9870d
ibrezm1 <ibrezm1@gmail.com>
2021-09-10 05:07:38 +0530

    initial

---
+++ test.js
@@ -0,0 +1,13 @@
+function ExampleComponent() {
+  return (
+    <div>
+      <h1>Hi</h1>
+      <p>hey</p>
+      <div className="special"></div>
+    </div>
+  )
+}
+
+
+$ git config --global user.name "ibrezm1"
+$ git config --global user.email ibrezm1@gmail.com
